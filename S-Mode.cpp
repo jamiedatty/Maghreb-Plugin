8b54912dea9e6280f605ea061397401370cfa97b
Jamie Datson <145495432+jamiedatty@users.noreply.github.com>
2026-06-07 10:13:19 +0100

    fix?

---
+++ S-Mode.cpp
@@ -0,0 +1,20 @@
+#include <windows.h>
+#include <string>
+#include <objidl.h>   
+#include <gdiplus.h>
+#include "EuroScopePlugIn.h"
+#include "TopSkyFunctions.h"
+#include "S-Mode.h"
+#include "IndraApcInterop.h"
+#include <map>
+#include <nlohmann/json.hpp>
+#include <iostream>
+#include <string>
+
+using namespace EuroScopePlugIn;
+using namespace Gdiplus;
+using namespace std;
+
+void Fetch_Json_data(char airport, char runway, char fix){
+    
+}
